4743f7ff36324daef59ca86633b1fc2772f722ca
shaswat2031 <prasadshaswat9265@gmail.com>
2025-03-10 22:23:05 +0530

    fix: update favicon link and change API base URL for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://chat-app-k703.onrender.com/api",
   withCredentials: true, // Allows sending cookies with requests
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://chat-app-k703.onrender.com" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
