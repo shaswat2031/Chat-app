419cbf53041e1cdbfc42c11909c1b469b1db9150
shaswat2031 <prasadshaswat9265@gmail.com>
2025-03-11 10:00:39 +0530

    fix: update import paths for database and route modules to use the src directory

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
-import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+import { connectDB } from "./src/lib/db.js";
+import authRoutes from "./src/routes/auth.route.js";
+import messageRoutes from "./src/routes/message.route.js";
+import { app, server } from "./src/lib/socket.js";
 
 dotenv.config();
 
